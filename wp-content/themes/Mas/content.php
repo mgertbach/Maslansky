2be8581aec75e95e1c8db6d7a3cac50f52b8b4be
Molly Gertenbach <mollygertenbach@Mollys-MacBook-Pro.local>
2020-03-23 15:30:59 -0400

    updated blog feed & page styles

--- wp-content/themes/Mas/content.php
+++ wp-content/themes/Mas/content.php
@@ -8,64 +8,77 @@
 ?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<?php if ( is_sticky() && is_home() && ! is_paged() ) { ?>
-			<div class="featured-post">
-				<?php esc_html_e( 'Featured post', 'mas' ); ?>
+		<div class="col grid_5_of_12" id="post-sidebar">
+			<div class="row">
+				<header class="entry-header">
+					<?php if ( is_single() ) { ?>
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					<?php }
+					else { ?>
+						<h1 class="entry-title">
+							<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to ', 'mas' ) . '%s', the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+						</h1>
+					<?php } // is_single() ?>
+					<?php mas_posted_on(); ?>
+					<?php //if ( has_post_thumbnail() && !is_search() ) { ?>
+						<!-- <a href="<?php //the_permalink(); ?>" title="<?php //echo esc_attr( sprintf( esc_html__( 'Permalink to ', 'mas' ) . '%s', the_title_attribute( 'echo=0' ) ) ); ?>"> -->
+							<?php //the_post_thumbnail( 'post_feature_full_width' ); ?>
+						<!-- </a> -->
+					<?php //} ?>
+				</header> <!-- /.entry-header -->
 			</div>
-		<?php } ?>
-		<header class="entry-header">
-			<?php if ( is_single() ) { ?>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-			<?php }
-			else { ?>
-				<h1 class="entry-title">
-					<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to ', 'mas' ) . '%s', the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-				</h1>
-			<?php } // is_single() ?>
-			<?php mas_posted_on(); ?>
-			<?php if ( has_post_thumbnail() && !is_search() ) { ?>
-				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to ', 'mas' ) . '%s', the_title_attribute( 'echo=0' ) ) ); ?>">
-					<?php the_post_thumbnail( 'post_feature_full_width' ); ?>
-				</a>
-			<?php } ?>
-		</header> <!-- /.entry-header -->
+		</div>
+		<div class="col grid_7_of_12" id="post-content">
+			<div class="row">
+				<?php if ( is_sticky() && is_home() && ! is_paged() ) { ?>
+					<div class="featured-post">
+						<?php esc_html_e( 'Featured post', 'mas' ); ?>
+					</div>
+				<?php } ?>
 
-		<?php if ( is_search() ) { // Only display Excerpts for Search ?>
-			<div class="entry-summary">
-				<?php the_excerpt(); ?>
-			</div> <!-- /.entry-summary -->
-		<?php }
-		else { ?>
-			<div class="entry-content">
-				<?php if ( has_excerpt() && !is_single() ) {
-					the_excerpt(); ?>
-					<p><a class="more-link" href="<?php the_permalink(); ?>"><?php echo wp_kses( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'mas' ), array( 'span' => array(
-						'class' => array() ) ) ) ?></a></p>
+
+				<?php if ( is_search() ) { // Only display Excerpts for Search ?>
+					<div class="entry-summary">
+						<?php the_excerpt(); ?>
+					</div> <!-- /.entry-summary -->
 				<?php }
-				else {
-					the_content( wp_kses( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'mas' ), array( 'span' => array(
-						'class' => array() ) ) )
-						);
-				}
-				 ?>
-				<?php wp_link_pages( array(
-					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'mas' ),
-					'after' => '</div>',
-					'link_before' => '<span class="page-numbers">',
-					'link_after' => '</span>'
-				) ); ?>
-			</div> <!-- /.entry-content -->
-		<?php } ?>
+				else { ?>
+					<div class="entry-content">
+						<?php if ( has_excerpt() && !is_single() ) {
+							the_excerpt(); ?>
+							<p><a class="more-link" href="<?php the_permalink(); ?>"><?php echo wp_kses( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'mas' ), array( 'span' => array(
+								'class' => array() ) ) ) ?></a></p>
+						<?php }
+						else {
+							the_content( wp_kses( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'mas' ), array( 'span' => array(
+								'class' => array() ) ) )
+								);
+						}
+						 ?>
+						<?php wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'mas' ),
+							'after' => '</div>',
+							'link_before' => '<span class="page-numbers">',
+							'link_after' => '</span>'
+						) ); ?>
+					</div> <!-- /.entry-content -->
+				<?php } ?>
+				<div class="row post-share">
+					<label id="share_label" class="col mas-graphik-light color-dark type_xs">Share</label>
+					<?php wp_nav_menu( array( 'theme_location' => 'social', 'container_class' => 'menu-social-links-menu-container col link-color-dark' ) ); ?>
+				</div>
+				<footer class="entry-meta">
 
-		<footer class="entry-meta">
-			<?php if ( is_singular() ) {
-				// Only show the tags on the Single Post page
-				mas_entry_meta();
-			} ?>
-			<?php edit_post_link( esc_html__( 'Edit', 'mas' ) . ' <i class="fa fa-angle-right" aria-hidden="true"></i>', '<div class="edit-link">', '</div>' ); ?>
-			<?php if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) {
-				// If a user has filled out their description and this is a multi-author blog, show their bio
-				get_template_part( 'author-bio' );
-			} ?>
-		</footer> <!-- /.entry-meta -->
+					<?php if ( is_singular() ) {
+						// Only show the tags on the Single Post page
+						mas_entry_meta();
+					} ?>
+					<?php edit_post_link( esc_html__( 'Edit', 'mas' ) . ' <i class="fa fa-angle-right" aria-hidden="true"></i>', '<div class="edit-link">', '</div>' ); ?>
+					<?php if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) {
+						// If a user has filled out their description and this is a multi-author blog, show their bio
+						get_template_part( 'author-bio' );
+					} ?>
+				</footer> <!-- /.entry-meta -->
+			</div>
+		</div>
 	</article> <!-- /#post -->
--- wp-content/themes/Mas/functions.php
+++ wp-content/themes/Mas/functions.php
@@ -677,76 +677,80 @@ add_action( 'comment_form_field_comment', 'mas_comment_form_field_comment' );
 if ( ! function_exists( 'mas_posted_on' ) ) {
 	function mas_posted_on() {
 		$post_icon = '';
-		switch ( get_post_format() ) {
-			case 'aside':
-				$post_icon = 'fa-file-o';
-				break;
-			case 'audio':
-				$post_icon = 'fa-volume-up';
-				break;
-			case 'chat':
-				$post_icon = 'fa-comment';
-				break;
-			case 'gallery':
-				$post_icon = 'fa-camera';
-				break;
-			case 'image':
-				$post_icon = 'fa-picture-o';
-				break;
-			case 'link':
-				$post_icon = 'fa-link';
-				break;
-			case 'quote':
-				$post_icon = 'fa-quote-left';
-				break;
-			case 'status':
-				$post_icon = 'fa-user';
-				break;
-			case 'video':
-				$post_icon = 'fa-video-camera';
-				break;
-			default:
-				$post_icon = 'fa-calendar';
-				break;
-		}
+		// switch ( get_post_format() ) {
+		// 	case 'aside':
+		// 		$post_icon = 'fa-file-o';
+		// 		break;
+		// 	case 'audio':
+		// 		$post_icon = 'fa-volume-up';
+		// 		break;
+		// 	case 'chat':
+		// 		$post_icon = 'fa-comment';
+		// 		break;
+		// 	case 'gallery':
+		// 		$post_icon = 'fa-camera';
+		// 		break;
+		// 	case 'image':
+		// 		$post_icon = 'fa-picture-o';
+		// 		break;
+		// 	case 'link':
+		// 		$post_icon = 'fa-link';
+		// 		break;
+		// 	case 'quote':
+		// 		$post_icon = 'fa-quote-left';
+		// 		break;
+		// 	case 'status':
+		// 		$post_icon = 'fa-user';
+		// 		break;
+		// 	case 'video':
+		// 		$post_icon = 'fa-video-camera';
+		// 		break;
+		// 	default:
+		// 		$post_icon = 'fa-calendar';
+		// 		break;
+		// }
 
 		// Translators: 1: Icon 2: Permalink 3: Post date and time 4: Publish date in ISO format 5: Post date
-		$date = sprintf( '<i class="fa %1$s" aria-hidden="true"></i> <a href="%2$s" title="Posted %3$s" rel="bookmark"><time class="entry-date" datetime="%4$s" itemprop="datePublished">%5$s</time></a>',
-			$post_icon,
-			esc_url( get_permalink() ),
-			sprintf( esc_html__( '%1$s @ %2$s', 'mas' ), esc_html( get_the_date() ), esc_attr( get_the_time() ) ),
-			esc_attr( get_the_date( 'c' ) ),
-			esc_html( get_the_date() )
-		);
+		// $date = sprintf( '<i class="fa %1$s" aria-hidden="true"></i> <a href="%2$s" title="Posted %3$s" rel="bookmark"><time class="entry-date" datetime="%4$s" itemprop="datePublished">%5$s</time></a>',
+		// 	$post_icon,
+		// 	esc_url( get_permalink() ),
+		// 	sprintf( esc_html__( '%1$s @ %2$s', 'mas' ), esc_html( get_the_date() ), esc_attr( get_the_time() ) ),
+		// 	esc_attr( get_the_date( 'c' ) ),
+		// 	esc_html( get_the_date() )
+		// );
+		// $date =
+
+		echo "<div id='post_date'><span class='type_xs color_l3 mas-graphik-light font-lighter'>".esc_html( get_the_date() )."</span></div>";
 
 		// Translators: 1: Date link 2: Author link 3: Categories 4: No. of Comments
-		$author = sprintf( '<i class="fa fa-pencil" aria-hidden="true"></i> <address class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></address>',
-			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-			esc_attr( sprintf( esc_html__( 'View all posts by %s', 'mas' ), get_the_author() ) ),
-			get_the_author()
-		);
+		// $author = sprintf( '<i class="fa fa-pencil" aria-hidden="true"></i> <address class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></address>',
+		// 	esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+		// 	esc_attr( sprintf( esc_html__( 'View all posts by %s', 'mas' ), get_the_author() ) ),
+		// 	get_the_author()
+		// );
 
 		// Return the Categories as a list
-		$categories_list = get_the_category_list( esc_html__( ' ', 'mas' ) );
+		// $categories_list = get_the_category_list( esc_html__( ' ', 'mas' ) );
 
 		// Translators: 1: Permalink 2: Title 3: No. of Comments
-		$comments = sprintf( '<span class="comments-link"><i class="fa fa-comment" aria-hidden="true"></i> <a href="%1$s" title="%2$s">%3$s</a></span>',
-			esc_url( get_comments_link() ),
-			esc_attr( esc_html__( 'Comment on ' , 'mas' ) . the_title_attribute( 'echo=0' ) ),
-			( get_comments_number() > 0 ? sprintf( _n( '%1$s Comment', '%1$s Comments', get_comments_number(), 'mas' ), get_comments_number() ) : esc_html__( 'No Comments', 'mas' ) )
-		);
+		// $comments = sprintf( '<span class="comments-link"><i class="fa fa-comment" aria-hidden="true"></i> <a href="%1$s" title="%2$s">%3$s</a></span>',
+		// 	esc_url( get_comments_link() ),
+		// 	esc_attr( esc_html__( 'Comment on ' , 'mas' ) . the_title_attribute( 'echo=0' ) ),
+		// 	( get_comments_number() > 0 ? sprintf( _n( '%1$s Comment', '%1$s Comments', get_comments_number(), 'mas' ), get_comments_number() ) : esc_html__( 'No Comments', 'mas' ) )
+		// );
 
 		// Translators: 1: Date 2: Author 3: Categories 4: Comments
-		printf( wp_kses( __( '<div class="header-meta">%1$s%2$s<span class="post-categories">%3$s</span>%4$s</div>', 'mas' ), array(
-			'div' => array (
-				'class' => array() ),
-			'span' => array(
-				'class' => array() ) ) ),
-			$date,
-			$author,
-			$categories_list,
-			( is_search() ? '' : $comments )
-		);
+		// echo $date;
+		// printf( wp_kses( __( '<div class="header-meta">%1$s%2$s<span class="post-categories">%3$s</span>%4$s</div>', 'mas' ), array(
+		// 	'div' => array (
+		// 		'class' => array() ),
+		// 	'span' => array(
+		// 		'class' => array() ) ) ),
+		// 	$date
+		// 	// $author,
+		// 	// $categories_list,
+		// 	// ( is_search() ? '' : $comments )
+		// );
 	}
 }
 
